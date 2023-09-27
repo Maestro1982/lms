8ef519c843ace31c08fa3fb6f6e57957a45301dd
Maestro1982 <kris.tote@telenet.be>
2023-09-27 15:17:59 +0200

    modify page.tsx in app dashboard routes teacher courses courseId folder, create attachment-form.tsx and price-form.tsx in app dashboard routes teacher courses courseId _components folder, route.ts in app api courses courseId attachments folder, route.ts in app api courses courseId attachments attachmentId folder, format.ts in lib folder

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -1,6 +1,6 @@
 import { auth } from '@clerk/nextjs';
 import { redirect } from 'next/navigation';
-import { LayoutDashboard } from 'lucide-react';
+import { LayoutDashboard, ListChecks, EuroIcon, File } from 'lucide-react';
 
 import { db } from '@/lib/db';
 
@@ -10,6 +10,8 @@ import { TitleForm } from './_components/title-form';
 import { DescriptionForm } from './_components/description-form';
 import { ImageForm } from './_components/image-form';
 import { CategoryForm } from './_components/category-form';
+import { PriceForm } from './_components/price-form';
+import { AttachmentForm } from './_components/attachment-form';
 
 const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
   const { userId } = auth();
@@ -21,6 +23,13 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
   const course = await db.course.findUnique({
     where: {
       id: params.courseId,
+    },
+    include: {
+      attachments: {
+        orderBy: {
+          createdAt: 'desc',
+        },
+      },
     },
   });
 
@@ -74,6 +83,29 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
               value: category.id,
             }))}
           />
+        </div>
+        <div className='space-y-6'>
+          <div>
+            <div className='flex items-center gap-x-2'>
+              <IconBadge icon={ListChecks} />
+              <h2 className='text-xl'>Course chapters</h2>
+            </div>
+            <div>TODO: Chapters</div>
+          </div>
+          <div>
+            <div className='flex items-center gap-x-2'>
+              <IconBadge icon={EuroIcon} />
+              <h2 className='text-xl'>Sell your course</h2>
+            </div>
+            <PriceForm initialData={course} courseId={course.id} />
+          </div>
+          <div>
+            <div className='flex items-center gap-x-2'>
+              <IconBadge icon={File} />
+              <h2 className='text-xl'>Resources & Attachments</h2>
+            </div>
+            <AttachmentForm initialData={course} courseId={course.id} />
+          </div>
         </div>
       </div>
     </div>
