8d07fdc8de36893d9d21b3948e6adf736d5ce165
Maestro1982 <kris.tote@telenet.be>
2023-10-03 12:06:48 +0200

    modify chapter-actions.tsx in app dashboard routes teacher courses courseId chapters chapterId _components folder, route.ts in app api courses courseId chapters chapterId folder, create route.ts in app api courses courseId chapters chapterId publish folder, route.ts in app api courses courseId chapters chapterId unpublish folder

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/_components/chapter-actions.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/_components/chapter-actions.tsx
@@ -25,6 +25,29 @@ export const ChapterActions = ({
   const [isLoading, setIsLoading] = useState(false);
   const router = useRouter();
 
+  const onPublish = async () => {
+    try {
+      setIsLoading(true);
+
+      if (isPublished) {
+        await axios.patch(
+          `/api/courses/${courseId}/chapters/${chapterId}/unpublish`
+        );
+        toast.success('Chapter unpublished');
+      } else {
+        await axios.patch(
+          `/api/courses/${courseId}/chapters/${chapterId}/publish`
+        );
+        toast.success('Chapter published');
+      }
+      router.refresh();
+    } catch {
+      toast.error('Something went wrong');
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   const onDelete = async () => {
     try {
       setIsLoading(true);
@@ -41,7 +64,7 @@ export const ChapterActions = ({
   return (
     <div className='flex items-center gap-x-2'>
       <Button
-        onClick={() => {}}
+        onClick={onPublish}
         disabled={disabled || isLoading}
         variant='outline'
         size='sm'
